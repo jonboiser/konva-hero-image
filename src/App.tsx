c0744e2311c7e298f46938e0f5f494ef38b3c329
Jonathan Boiser <jonboiser@users.noreply.github.com>
2025-01-09 09:57:43 -0800

    add fake rectangles

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import { Stage, Image, Layer, Rect, Text } from 'react-konva';
 import './App.css';
 import Konva from 'konva';
@@ -14,18 +14,20 @@ const ColoredRect = () => {
 	return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={handleClick} />;
 };
 
-const UnitImage = () => {
+const UnitImage = forwardRef<Konva.Image>((props, ref) => {
 	const [image] = useImage(
 		'https://imgin-stage.instrumental.ai/files%2Furn%3Adfx%3Aproject%3A5%2Furn%3Adfx%3Afile%3A3768732%2FPVT-DOE-JDR61762142SLA17X-SOLAR-170220111658.jpg?q=60&s=TzAvOclWasdwiqqQ33cq%2F7SmbPBkhBfANnhu%2FmeMHuE%3D&instck=2bef41cffe1bd5c92ff573019ec5aecb',
 	);
-	return <Image image={image} />;
-};
+	return <Image ref={ref} image={image} />;
+});
 
 // render(<App />, document.getElementById('root'));
 function App() {
 	const [stageScale, setStageScale] = useState(1);
 	const [stageX, setStageX] = useState(0);
 	const [stageY, setStageY] = useState(0);
+	const imgRef = useRef<Konva.Image>(null);
+	const [rectanglesProps, setRectanglesProps] = useState<Konva.RectConfig[]>([]);
 
 	const handleWheel = (e: Konva.KonvaEventObject<WheelEvent>) => {
 		e.evt.preventDefault();
@@ -49,6 +51,21 @@ function App() {
 		setStageY(pointer.y - mousePointTo.y * newScale);
 	};
 
+	if (rectanglesProps.length === 0 && imgRef.current) {
+		const width = imgRef.current.getWidth() - 420;
+		const height = imgRef.current.getHeight() - 420;
+		setRectanglesProps(
+			Array.from({ length: 20 }, () => ({
+				x: Math.random() * width,
+				y: Math.random() * height,
+				width: Math.random() * 400 + 20,
+				height: Math.random() * 400 + 20,
+				fill: Konva.Util.getRandomColor(),
+				opacity: 0.5,
+			})),
+		);
+	}
+
 	return (
 		<>
 			<div>
@@ -64,7 +81,12 @@ function App() {
 					onWheel={handleWheel}
 				>
 					<Layer>
-						<UnitImage />
+						<UnitImage ref={imgRef} />
+					</Layer>
+					<Layer>
+						{rectanglesProps.map((props, i) => (
+							<Rect key={i} {...props} />
+						))}
 					</Layer>
 				</Stage>
 			</div>
