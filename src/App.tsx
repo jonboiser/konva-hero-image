7ad6c06eec6f8f159e03791f8dc66d49adc779ea
Jonathan Boiser <jonboiser@users.noreply.github.com>
2025-01-09 09:31:27 -0800

    konva

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,31 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { Stage, Layer, Rect, Text } from 'react-konva';
 import './App.css';
+import Konva from 'konva';
 
+const ColoredRect = () => {
+	const [color, setColor] = useState('green');
+
+	const handleClick = () => {
+		setColor(Konva.Util.getRandomColor());
+	};
+
+	return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={handleClick} />;
+};
+
+// render(<App />, document.getElementById('root'));
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+				<h1>Konva Prototype</h1>
+				<Stage width={window.innerWidth} height={window.innerHeight}>
+					<Layer>
+						<Text text="Try click on rect" />
+						<ColoredRect />
+					</Layer>
+				</Stage>
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
 		</>
 	);
 }
