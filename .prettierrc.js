67df51134c2053fa26e3ae77eb955bb6497341f7
Jonathan Boiser <jonboiser@users.noreply.github.com>
2025-01-09 09:26:24 -0800

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/** @type {import('prettier').Config} */
+const config = {
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  useTabs: true,
+  tabWidth: 4,
+};
+
+export default config;
