4eb8ae95224935d1b49a479f11ea66968af2b55f
Jonathan Boiser <jonboiser@users.noreply.github.com>
2025-01-09 09:36:03 -0800

    add image

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react';
-import { Stage, Layer, Rect, Text } from 'react-konva';
+import { Stage, Image, Layer, Rect, Text } from 'react-konva';
 import './App.css';
 import Konva from 'konva';
+import useImage from 'use-image';
 
 const ColoredRect = () => {
 	const [color, setColor] = useState('green');
@@ -13,6 +14,13 @@ const ColoredRect = () => {
 	return <Rect x={20} y={20} width={50} height={50} fill={color} shadowBlur={5} onClick={handleClick} />;
 };
 
+const LionImage = () => {
+	const [image] = useImage(
+		'https://imgin-stage.instrumental.ai/files%2Furn%3Adfx%3Aproject%3A5%2Furn%3Adfx%3Afile%3A3768732%2FPVT-DOE-JDR61762142SLA17X-SOLAR-170220111658.jpg?q=60&s=TzAvOclWasdwiqqQ33cq%2F7SmbPBkhBfANnhu%2FmeMHuE%3D&instck=2bef41cffe1bd5c92ff573019ec5aecb',
+	);
+	return <Image image={image} />;
+};
+
 // render(<App />, document.getElementById('root'));
 function App() {
 	return (
@@ -21,6 +29,7 @@ function App() {
 				<h1>Konva Prototype</h1>
 				<Stage width={window.innerWidth} height={window.innerHeight}>
 					<Layer>
+						<LionImage />
 						<Text text="Try click on rect" />
 						<ColoredRect />
 					</Layer>
